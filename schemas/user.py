6158b5a0828f0dacfec433a10266a5425e7f10db
Youssef Galal <youssefgalalnazem@gmail.com>
2022-10-27 18:34:59 +0200

    Remove email from user model

--- schemas/user.py
+++ schemas/user.py
@@ -3,13 +3,11 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     name: str
-    email: str
     password: str
 
 
 class ShowUser(BaseModel):
     name: str
-    email: str
 
     class Config():
         orm_mode = True
