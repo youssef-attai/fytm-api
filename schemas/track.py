2c0c6f57c7eacc85c91bb100089bc21b2016807f
Youssef Galal <youssefgalalnazem@gmail.com>
2022-10-28 11:19:57 +0200

    Add TODO
    audio_url should be removed from schema and added to Track(Base) instances
        #  via pytube in the track_router

--- schemas/track.py
+++ schemas/track.py
@@ -7,4 +7,7 @@ class Track(BaseModel):
     artist: int
     album: int
     youtube: str
+
+    # TODO: Should be removed from schema and added to Track(Base) instances
+    #  via pytube in the track_router
     audio_url: str
