944286c192c823ac3796d8bbd4b0271615d36d70
Youssef Galal <youssefgalalnazem@gmail.com>
2022-10-28 11:15:40 +0200

    refactoring

--- routers/user.py
+++ routers/user.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
-
-from schemas import user as user_schemas
-from repository import user as user_repository
-from database import get_db
 from sqlalchemy.orm import Session
+
+from database import get_db
+from repository import user as user_repository
+from schemas import user as user_schemas
 
 router = APIRouter(
     prefix="/user",
@@ -16,6 +16,6 @@ def create_user(request: user_schemas.User, db: Session = Depends(get_db)):
     return user_repository.create(request, db)
 
 
-@router.get('/{id}', response_model=user_schemas.ShowUser)
-def get_user_by_id(id: int, db: Session = Depends(get_db)):
-    return user_repository.get_by_id(id, db)
+@router.get('/{uid}', response_model=user_schemas.ShowUser)
+def get_user_by_id(uid: int, db: Session = Depends(get_db)):
+    return user_repository.get_by_id(uid, db)
