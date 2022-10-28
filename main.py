2dd4391dfeb3071d0704ea5da0814db10ad4aaf3
Youssef Galal <youssefgalalnazem@gmail.com>
2022-10-28 10:14:29 +0200

    Add new routers

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from routers import user as user_router
 from routers import auth as auth_router
 from routers import track as track_router
+from routers import artist as artist_router
+from routers import album as album_router
 from database import engine, Base
 
 
@@ -10,6 +12,8 @@ app = FastAPI()
 
 Base.metadata.create_all(engine)
 
-app.include_router(user_router.router)
 app.include_router(auth_router.router)
+app.include_router(user_router.router)
 app.include_router(track_router.router)
+app.include_router(artist_router.router)
+app.include_router(album_router.router)
