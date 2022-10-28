3b1dd1a905ed0d9743612f51bde609e39d05b96b
Youssef Galal <youssefgalalnazem@gmail.com>
2022-10-28 10:13:32 +0200

    Add /track create endpoint

--- routers/track.py
+++ routers/track.py
@@ -1,14 +1,23 @@
-from fastapi import APIRouter
-from schemas import user as user_schema
+from fastapi import APIRouter, status
 from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from database import get_db
 from oauth2 import get_current_user
 from repository import track as track_repository
+from schemas import track as track_schema
+from schemas import user as user_schema
 
 router = APIRouter(
     prefix="/track",
     tags=["tracks"],
 )
 
-@router.get('/')
-def my_test(current_user: user_schema.User = Depends(get_current_user)):
-    return track_repository.my_test()
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create_track(
+        request: track_schema.Track,
+        db: Session = Depends(get_db),
+        current_user: user_schema.User = Depends(get_current_user)
+):
+    return track_repository.create(request, db)
