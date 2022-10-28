c12afa8a4c8ba1f66f84d84555dfc4825eb8f93b
Youssef Galal <youssefgalalnazem@gmail.com>
2022-10-28 20:32:29 +0200

    Improve artist creation
    - Check if artist already exists
    - Return ShowArtist object

--- repository/artist.py
+++ repository/artist.py
@@ -1,4 +1,4 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from database import get_db
@@ -7,10 +7,22 @@ from schemas import artist as artist_schema
 
 
 def create(artist: artist_schema.Artist, db: Session = Depends(get_db)):
+    already_exists = db.query(artist_model.Artist).filter(artist_model.Artist.name == artist.name).first()
+    if already_exists is not None:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Artist \"{artist.name}\" already exists"
+        )
+
     new_artist = artist_model.Artist(
         name=artist.name
     )
+
     db.add(new_artist)
     db.commit()
     db.refresh(new_artist)
-    return new_artist
+
+    return artist_schema.ShowArtist(
+        artist_id=new_artist.id,
+        name=new_artist.name
+    )
