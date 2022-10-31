f26732e6fc2bffde4723bba5c5d694ddf9fe89a4
Youssef Galal <youssefgalalnazem@gmail.com>
2022-10-31 21:28:24 +0200

    Make Track model a dataclass

--- schemas/track.py
+++ schemas/track.py
@@ -1,7 +1,11 @@
 from pydantic import BaseModel
 
+from pydantic.dataclasses import dataclass
 
+
+@dataclass(eq=True, frozen=True)
 class Track(BaseModel):
+    watch_id: str
     title: str
     author: str
     thumbnail_url: str
