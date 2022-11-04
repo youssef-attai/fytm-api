5273b3faad8f8ae0ec1dee38387f1e85e2688cc0
Youssef Galal <youssefgalalnazem@gmail.com>
2022-11-04 08:08:57 +0200

    Create favorites database

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from deta_shared import deta
 
 users_db = deta.Base(name='users')
+favorites_db = deta.Base(name='favorites')
 
 
 def get_users_db():
     return users_db
+
+
+def get_favorites_db():
+    return favorites_db
