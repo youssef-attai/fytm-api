60f2ab8ce283318340b14ea24ca5967e551247e3
Youssef Galal <youssefgalalnazem@gmail.com>
2022-11-05 06:48:40 +0200

    Cache requested tracks

--- routers/track.py
+++ routers/track.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from deta import _Base
+from fastapi import APIRouter, Depends
 
-from utils import create_track_from_watch_id
+from database import get_track_cache_db
+from utils import cache_track
 
 router = APIRouter(
     prefix="/track",
@@ -9,5 +11,15 @@ router = APIRouter(
 
 
 @router.get('/{watch_id}')
-async def track(watch_id: str):
-    return create_track_from_watch_id(watch_id)
+async def track(watch_id: str, track_cache_db: _Base = Depends(get_track_cache_db)):
+    cached_track = track_cache_db.fetch({
+        "watch_id": watch_id
+    }).items
+
+    # If track is not in cached db
+    if len(cached_track) == 0:
+        # Get track from YouTube
+        return cache_track(watch_id, track_cache_db)
+
+    # If track is cached
+    return cached_track[0]
