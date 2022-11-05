a7dec75ff6bd4e0d4225d1031f03c1f762f49c62
Youssef Galal <youssefgalalnazem@gmail.com>
2022-11-05 06:49:00 +0200

    Create track cache database

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from deta_shared import deta
 
 users_db = deta.Base(name='users')
-favorites_db = deta.Base(name='favorites')
+track_cache_db = deta.Base(name='track_cache')
 
 
 def get_users_db():
     return users_db
 
 
-def get_favorites_db():
-    return favorites_db
+def get_track_cache_db():
+    return track_cache_db
